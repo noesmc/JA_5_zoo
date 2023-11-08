50951e8677c80c1e40faac3a5d49139e702f4d0a
Синюкович Никита Андреевич <mikita.siniukovich@softclub.by>
2023-11-08 17:35:36 +0300

    JA_5_zoo

--- src/animals/Dog.java
+++ src/animals/Dog.java
@@ -3,16 +3,22 @@ package animals;
 import java.util.Scanner;
 
 public class Dog extends Animals {
-    public Dog(String kind,String name, String colour, boolean tail, boolean mane) {
+    public Dog(String kind, String name, String colour, boolean tail, boolean mane) {
         super(kind, name, colour, tail, mane);
     }
+
     public void feed() {
         Scanner scanner = new Scanner((System.in));
         String feed;
         System.out.printf("What do u wanna give to %s? Meat or bones?", getName());
         feed = scanner.nextLine();
         System.out.printf("%s", feed.equalsIgnoreCase("meat") ?
-                "Doesn't want meat, sorry :(" : (feed.equalsIgnoreCase("bones") ?
-                "Bingo, just what she wanted!" : "You offer shit, doesn't want anything anymore."));
+            "Doesn't want meat, sorry :(" : (feed.equalsIgnoreCase("bones") ?
+            "Bingo, just what she wanted!" : "You offer shit, doesn't want anything anymore."));
+    }
+
+    @Override
+    public void movement() {
+        System.out.printf("%s moves on five legs. ", getName());
     }
 }
